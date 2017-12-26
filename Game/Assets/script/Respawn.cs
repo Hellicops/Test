f8f68e59b3238beaadc62ded102cbc52c545e171
F. Sharafutdinov <hellicops@mail.com>
2017-12-27 02:55:32 +0600

    Звуки и ловушки

---
+++ Game/Assets/script/Respawn.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Respawn : MonoBehaviour
+{
+    private BoxCollider2D Collider;
+    private void Start()
+    {
+        Collider = GetComponent<BoxCollider2D>();
+    }
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        unit unit = collider.GetComponent<unit>();
+        if(unit && unit is player)
+        {
+            Collider.enabled = false;
+        }
+    }
+
+}
---
+++ Game/Assets/script/mage.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mage : MonoBehaviour {
+    private Vector3 position;
+    public experiense experiense;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+    private void OnDestroy()
+    {
+        Instantiate(experiense, position, transform.rotation);
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        position = transform.position;
+        position = new Vector3(transform.position.x, transform.position.y + 1f);
+    }
+}
